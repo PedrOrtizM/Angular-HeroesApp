32b54fc5deb5c7dde2abf800706ccae67ca55361
PedrOrtizM <pedrortizm18_@hotmail.com>
2019-01-28 17:14:03 -0500

    proyecto finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
+
+
+import { HttpClientModule }  from "@angular/common/http";
+import { FormsModule }  from "@angular/forms";
+import { KeysPipe } from './pipes/keys.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent,
+    KeysPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
